6fcaede2acf7d2bbc57777bf89e3f77a12d42427
JamesDev209 <james1913_@hotmail.com>
2025-07-10 00:05:43 -0500

    se puso la ruta estaticatica v1

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { router } = require("./routes")
 app.use(cors());
 
 
+app.use('/v1', router)
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=> console.log('server corriendo', PORT))
